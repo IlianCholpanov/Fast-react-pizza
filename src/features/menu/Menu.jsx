286814656842153b43ce53ffbbcab27313ce598c
Ilian Cholpanov <iliancholpanov@gmail.com>
2024-04-26 23:13:50 +0300

    .

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,17 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+
 function Menu() {
+  const menu = useLoaderData();
+  console.log(menu);
+
   return <h1>Menu</h1>;
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
