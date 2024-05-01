925730f93bd052ac2d8591cf7e5fa221cc101b9c
Ilian Cholpanov <iliancholpanov@gmail.com>
2024-05-01 14:17:04 +0300

    .

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Ilko</div>;
+  return <div className="hidden text-sm font-semibold ">Ilko</div>; //md:block removed from the classname
 }
 
 export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
 
       <SearchOrder />
 
-      {/* <Username /> */}
+      <Username />
     </header>
   );
 }
