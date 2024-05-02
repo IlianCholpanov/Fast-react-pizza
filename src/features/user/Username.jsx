66adc5a5b3eb834c4ef59d4b8d9cc1fb159fe455
Ilian Cholpanov <iliancholpanov@gmail.com>
2024-05-02 23:44:30 +0300

    ..

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,10 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Ilko</div>; //md:block removed from the classname
+  const username = useSelector((state) => state.user.username);
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  ); //md:block removed from the classname
 }
 
 export default Username;
