98f6da82205d8a5f3c1a15e24b0a7763421e9451
Ilian Cholpanov <iliancholpanov@gmail.com>
2024-05-03 19:58:47 +0300

    .

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -25,7 +25,7 @@ async function fetchAddress() {
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  username: 'Ilkosite',
+  username: '',
 };
 
 const userSlice = createSlice({
